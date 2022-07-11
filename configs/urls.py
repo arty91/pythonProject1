3f7eafc010341e4d3973e4df1eefd321995c7e03
arty91 <artem.zherdev6@gmail.com>
2022-07-11 11:22:32 +0300

    django 2nd lesson vr1

--- configs/urls.py
+++ configs/urls.py
@@ -15,11 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from first.views import FirstView, SecondView, ThirdView
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    path('first', FirstView.as_view()),
-    path('second', SecondView.as_view()),
-    path('third/<str:name>/<int:age>', ThirdView.as_view())
 ]
