dd1d487b9f0de435865ab415f4973a37885c29fd
arty91 <artem.zherdev6@gmail.com>
2022-07-12 19:38:25 +0300

    django lesson2 hw2 vr1

--- apps/cars/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.serializers import ModelSerializer
-from .models import CarModel
-
-
-class CarSerializer(ModelSerializer):
-	class Meta:
-		model = CarModel
-		# fields = '__all__'
-		fields = ('id', 'brand', 'price', 'year')
--- apps/cars/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import CarListCreateView, CarUpdateretrieveDestroyView
-
-urlpatterns = [
-	path('', CarListCreateView.as_view()),
-	path('/<int:pk>', CarUpdateretrieveDestroyView.as_view())
-]
